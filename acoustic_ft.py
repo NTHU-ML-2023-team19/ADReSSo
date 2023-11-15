b148eef10ef12143e124f1166be63858731f3f32
nevikw39 <nevikw39@cs.nthu.edu.tw>
2023-11-16 06:59:02 +0800

    Disable truncation

--- acoustic_ft.py
+++ acoustic_ft.py
@@ -1,6 +1,7 @@
 #!/home/nevikw/miniconda3/envs/ml-project/bin/python
 
 import sys
+import warnings
 
 from datasets import load_dataset, Audio
 from transformers import (
@@ -13,6 +14,9 @@ from transformers import (
 import evaluate
 import numpy as np
 
+
+warnings.filterwarnings("ignore")
+
 MODEL_NAME = sys.argv[1] if len(sys.argv) > 1 else "facebook/wav2vec2-base"
 
 dataset = load_dataset("nevikw39/ADReSSo").cast_column(
@@ -24,8 +28,6 @@ encoded_dataset = dataset.map(
     lambda examples: feature_extractor(
         [x["array"] for x in examples["audio"]],
         sampling_rate=feature_extractor.sampling_rate,
-        max_length=16000,
-        truncation=True,
     ),
     remove_columns="audio",
     batched=True,
@@ -50,16 +52,16 @@ training_args = TrainingArguments(
     save_strategy="epoch",
     save_total_limit=1,
     learning_rate=3e-5,
-    per_device_train_batch_size=32,
-    gradient_accumulation_steps=4,
+    per_device_train_batch_size=16,
+    gradient_accumulation_steps=2,
     per_device_eval_batch_size=32,
     num_train_epochs=100,
     warmup_ratio=0.1,
     logging_steps=10,
     load_best_model_at_end=True,
     metric_for_best_model="accuracy",
-    push_to_hub=True,
-    hub_private_repo=True,
+    # push_to_hub=True,
+    # hub_private_repo=True,
 )
 
 trainer = Trainer(
@@ -78,4 +80,4 @@ trainer = Trainer(
 trainer.train()
 trainer.evaluate(encoded_dataset["test"])
 trainer.save_model("models/" + MODEL_NAME[MODEL_NAME.index("/") + 1 :] + "_ADReSSo")
-trainer.push_to_hub()
+# trainer.push_to_hub()
